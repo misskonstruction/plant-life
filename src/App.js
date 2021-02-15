fceff8141f83263c586c33cda2d2ce2caed547e2
misskonstruction <misskonstruction@gmail.com>
2021-02-15 16:42:46 -0500

    updated menu-items to navigate withRouterHOC

--- src/App.js
+++ src/App.js
@@ -1,11 +1,22 @@
 import React from 'react';
+import {Switch, Route} from 'react-router-dom';
 import './App.css';
 import HomePage from './pages/homepage/homepage.component';
+
+const SucculentsPage = () => (
+  <div>
+    <h1>Succulents Page</h1>
+  </div>
+  );
 
 function App() {
   return (
     <div>
-     <HomePage />
+      <Switch>
+        <Route exact path='/' component={HomePage}/>
+        <Route path='/succulents' component={SucculentsPage} />
+      </Switch>  
+      
     </div>
   );
 }
--- src/components/directory/directory.component.jsx
+++ src/components/directory/directory.component.jsx
@@ -20,27 +20,27 @@ class Directory extends React.Component {
           title: 'herbs',
           imageUrl: 'https://1.bp.blogspot.com/-Q45yu7kjIlc/YCV0P41C8xI/AAAAAAAAJQY/BGlf2uMi7L0K7neAqQZ_MLh928ClZkhSgCLcBGAsYHQ/s320/herbs.jpg',
           id: 2,
-          linkUrl: 'herbs'
+          linkUrl: ''
         },
         {
           title: 'vegetables',
           imageUrl: 'https://1.bp.blogspot.com/-fGLTd8dxIcc/YCVyzgZ7bmI/AAAAAAAAJP8/b7agcMDyJpwXkNpLHYd64HVG0iWu8QW2QCLcBGAsYHQ/s320/veggies.jpg',
           id: 3,
-          linkUrl: 'veggies'
+          linkUrl: ''
         },
         {
           title: 'indoor',
           imageUrl: 'https://1.bp.blogspot.com/-AE7TBTdVpeY/YCVyzPXaEJI/AAAAAAAAJPw/nxv047GOv74ZZlbTrtIGs1izQSm7ubd2QCLcBGAsYHQ/s320/indoor.jpg',
           size: 'large',
           id: 4,
-          linkUrl: 'indoor'
+          linkUrl: ''
         },
         {
           title: 'outdoor',
           imageUrl: 'https://lh3.googleusercontent.com/-L7LpavU1nRw/YCqt1AKSjZI/AAAAAAAAJTU/9COa97ufu1ErSYz3JE8nHJw8Lk34ncD7ACLcBGAsYHQ/outdoors.jpg',
           size: 'large',
           id: 5,
-          linkUrl: 'outdoor'
+          linkUrl: ''
         }
       ]
     };
